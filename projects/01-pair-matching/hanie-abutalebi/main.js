ac3fe37a96d7eb1890739b3b98ed9310b02b8cd1
Hanie <h.abutalebi2018@gmail.com>
2022-11-21 22:02:51 +0330

    pair-matching-game

---
+++ projects/01-pair-matching/hanie-abutalebi/main.js
@@ -0,0 +1,109 @@
+const selectClass = {
+  board: document.querySelector(".board"),
+  board_box: document.querySelector(".board-box"),
+  win: document.querySelector(".win"),
+};
+let flippedCards = 0;
+
+const shuffle = (array) => {
+  const newArray = [...array];
+  for (let index = newArray.length - 1; index > 0; index--) {
+    const randomIndex = Math.floor(Math.random() * (index + 1));
+    const original = newArray[index];
+    newArray[index] = newArray[randomIndex];
+    newArray[randomIndex] = original;
+  }
+  return newArray;
+};
+
+const selectRandom = (array, items) => {
+  const newArray = [...array];
+  const randomPicks = [];
+
+  for (let index = 0; index < items; index++) {
+    const randomIndex = Math.floor(Math.random() * newArray.length);
+    randomPicks.push(newArray[randomIndex]);
+    newArray.splice(randomIndex, 1);
+  }
+  return randomPicks;
+};
+
+const generateGame = () => {
+  const dimensions = selectClass.board.getAttribute("data-dimension");
+  if (dimensions % 2 !== 0) {
+    alert("The dimension should be an even number.Please check.");
+  }
+  const emojis = ["🌂", "🧶", "🎓", "🐹", "🐳", "🌴", "🦦", "🏹", "🎹", "🪐"];
+  const picks = selectRandom(emojis, (dimensions * dimensions) / 2);
+  const items = shuffle([...picks, ...picks]);
+  selectClass.board_box.innerHTML = `
+      <div class="board" style="grid-template-columns: repeat(${dimensions}, auto)">
+          ${items
+            .map(
+              (item) => `
+              <div class="card">
+                  <div class="card-front"></div>
+                  <div class="card-back">${item}</div>
+              </div>`
+            )
+            .join("")}
+     </div>
+  `;
+};
+//flipback card
+const flipBack = () => {
+  document.querySelectorAll(".card:not(.equal)").forEach((card) => {
+    card.classList.remove("flipped");
+  });
+  flippedCards = 0;
+};
+
+//win function
+const win = () => {
+  selectClass.win.innerHTML = `
+            <span class="win-inner">
+              congratulations ! <br /> You won! <br />
+            </span>
+        `;
+};
+
+const flipCard = (card) => {
+  flippedCards++;
+  //less than 2 card fliped
+  if (flippedCards <= 2) {
+    card.classList.add("flipped");
+  }
+  //exactly 2 card fliped
+  if (flippedCards === 2) {
+    const flippedCards = document.querySelectorAll(".flipped:not(.match)");
+
+    if (flippedCards[0].innerText === flippedCards[1].innerText) {
+      flippedCards[0].classList.add("equal");
+      flippedCards[1].classList.add("equal");
+    }
+    setTimeout(() => {
+      flipBack();
+    }, 1000);
+  }
+  // all cards fliped
+  if (!document.querySelectorAll(".card:not(.flipped)").length) {
+    setTimeout(() => {
+      win();
+    }, 500);
+  }
+};
+//event listener and restart button
+const setEventListeners = () => {
+  document.addEventListener("click", (event) => {
+    if (event.target.className.includes("card-front")) {
+      flipCard(event.target.parentElement);
+    } else if (event.target.className.includes("restart")) {
+      if (!document.querySelectorAll(".card:not(.flipped)").length) {
+        selectClass.win.innerHTML = ``;
+      }
+      generateGame();
+    }
+  });
+};
+generateGame();
+setEventListeners();
