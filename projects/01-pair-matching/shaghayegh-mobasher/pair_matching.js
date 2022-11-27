6538372bd5aaf19ccf2ad523464f8cc10942a3eb
ShMob <mobashers313@gmail.com>
2022-11-27 10:02:20 +0330

    Add fetch API for getting the number of columns/rows, fix code to accept dynamic columns/rows

--- projects/01-pair-matching/shaghayegh-mobasher/pair_matching.js
+++ projects/01-pair-matching/shaghayegh-mobasher/pair_matching.js
@@ -1,3 +1,10 @@
+"use strict";
+
+var row_col_num;
+var card_count;
+var score = 0;
+var last_card = -1;
+
 function delay(time) {
     return new Promise(resolve => setTimeout(resolve, time));
 }
@@ -15,22 +22,46 @@ function turn_cards_back(card_el, other_card_el){
 }
 
 
+
+async function get_number(){
+	const controller = new AbortController();
+	let timeout = 4000;
+  	const id = setTimeout(() => controller.abort(), timeout);
+  	
+	let num = await fetch('http://192.168.43.20:8000/playgame', 
+		{
+		method:"GET", 
+		signal: controller.signal
+		}
+	)
+	.then(req => req.json())
+	.then(req => {
+	    console.log(req);
+	    return req.dimension;	
+	    })
+	.catch(err => {
+		console.log(err);
+		return 8;
+	})
+	clearTimeout(id);
+	return num;
+}
+
 function showCard(card_el){
     turn_card_front(card_el);
-    state_el = document.getElementById("state");
-    state = Number(state_el.textContent);
-    if(state == -1){
-        state_el.innerHTML = card_el.id.toString()
+    if(last_card == -1){
+        last_card = card_el.id
     }
     else{
-        other_card_el = document.getElementById(`${state}`);
-        other_value = other_card_el.firstElementChild.textContent;
-        this_value = card_el.firstElementChild.textContent;
+        let other_card_el = document.getElementById(`${last_card}`);
+        let other_value = other_card_el.firstElementChild.textContent;
+        let this_value = card_el.firstElementChild.textContent;
         if(other_value == this_value){
-            document.getElementById("score").innerHTML = 1 + parseInt(document.getElementById("score").textContent);
+        	score += 1;
+            document.getElementById("score").innerHTML = score.toString();
             card_el.style.backgroundColor = "indianred";
             other_card_el.style.backgroundColor = "indianred";
-            if (document.getElementById("score").innerHTML == "32"){
+            if (score == card_count){
                 document.getElementById("win").style.removeProperty('visibility');
             }
         }
@@ -43,7 +74,7 @@ function showCard(card_el){
                 }
             );
         }
-        state_el.innerHTML = "-1";
+        last_card = -1;
     }
 }
 
@@ -55,18 +86,25 @@ function shuffleArray(array) {
     return array
 }
 
-function startOver(){
-    columns = 8; //style of the cards is hardcoded with 8 columns, so other numbers for columns won't change the number of columns displayed.
-    rows = 8;
-    document.getElementById("state").innerHTML = "-1";
-    document.getElementById("score").innerHTML = "0";
-    document.getElementById("card-box").innerHTML = "";
+async function startOver(){
+	let cardbox = document.getElementById("card-box");
+    cardbox.innerHTML = '<h1 style="color:indianred;">Getting new cards...</h1>';
+    last_card = -1;
+    score = 0;
     document.getElementById("win").style.visibility = "hidden";
-    cards = shuffleArray([...Array(columns * rows / 2).keys()].map(n => (n+1).toString()).flatMap(i => [i,i]))
-    for(let i of Array(columns * rows).keys()){
-        document.getElementById("card-box").innerHTML += `<button class="card" onclick="showCard(this)" id="${i}">
-        <div class="character" style="visibility: hidden;">${cards[i]}</div>
-        </button>`;
+	row_col_num = await get_number();
+    let columns = row_col_num;
+    let rows = row_col_num;
+    card_count = parseInt(columns * rows / 2);
+    document.getElementById("score").innerHTML = score.toString();
+    cardbox.innerHTML = "";
+    cardbox.style.gridTemplateColumns = `repeat(${row_col_num}, 1fr)`;
+    cardbox.style.gridTemplateRows = `repeat(${row_col_num}, auto)`;
+    let cards = shuffleArray([...Array(card_count).keys()].map(n => (n+1).toString()).flatMap(i => [i,i]));
+    for(let i of Array(cards.length).keys()){
+	document.getElementById("card-box").innerHTML += `<button class="card" onclick="showCard(this)" id="${i}">
+	<div class="character" style="visibility: hidden;">${cards[i]}</div>
+	</button>`;
     }
 }
 
