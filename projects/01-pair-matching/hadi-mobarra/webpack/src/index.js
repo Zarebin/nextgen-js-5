cd7d44c822b8ed0de73ec6ba710c40289d971daf
hadimobarra <hadimobarra@gmail.com>
2022-12-12 18:44:17 +0330

    Eslint added

--- projects/01-pair-matching/hadi-mobarra/webpack/src/index.js
+++ projects/01-pair-matching/hadi-mobarra/webpack/src/index.js
@@ -1,135 +1,106 @@
-window.onload = function() {
+import './styles.css';
 
-	//Fetch API to get the number of row and column
-	async function chartNumberFunc() {
-		let chartNumber = await fetch('http://192.168.43.126:8000/playgame')
-			.then((response) => response.json()) 
-			.then((data) => {
-				return data.dimension;
-			})
-			.catch(err => alert(err));
-		return chartNumber;
-	}
-
-	// Array of numbers for Game 
-	let gameValues = [];
-	for (let i = 1; i <= 32; i++) {
-		gameValues.push(i);
-		gameValues.push(i);
-	}
-
-	// shuffle Array
-	gameValues.sort(() => 0.5 - Math.random());
-	let firstHalf = gameValues.slice(0, gameValues.length/2);
-	let secondHalf = gameValues.slice(gameValues.length/2, );
-	var card = document.createElement('div');
-    card.setAttribute('id', 'game');
-    document.getElementById('box').appendChild(card);
-    let newHalfTwo = gameValues.slice();
-
-	//Generate Table with 8*8 squares
-	async function tableGenerator() {
-		let chartNumber = await chartNumberFunc();
-		for (var i = 1; i <= chartNumber; i++) {
-			var cardItem = document.createElement('div');
-			cardItem.setAttribute('class', 'cardColumn');
-			cardItem.setAttribute('card-id', i);
-			card.appendChild(cardItem);
-			for (var j = 0; j < chartNumber; j++) {
-				var uniq = 'id' + (new Date()).getTime();
-				let arrLastElement = newHalfTwo.pop();
-				var cardItemTwo = document.createElement('div');
-				cardItemTwo.setAttribute('class', 'cardRow');
-				cardItemTwo.setAttribute('card-id', uniq );
-				cardItemTwo.setAttribute('id', 'cr');
-				cardItemTwo.setAttribute('inner', arrLastElement);
-				cardItemTwo.addEventListener('click', cardMatcher);
-				cardItemTwo.innerHTML = arrLastElement;
-				cardItem.appendChild(cardItemTwo);
-			}
-		}
-	}
-
-	tableGenerator();
-
-	// Change inner Value of each square to not be visible
-	setTimeout( () => {
-		let divs = document.getElementsByTagName('div'), i = divs.length;
-		while(--i) {
-			if (divs[i].getAttribute('class') == 'cardRow') {
-				divs[i].innerHTML = '?'
-			}
-		}
-	}, 5000);
-
-	let choosenCards = [];
-	let choosenId = [];
-	let trueChoosenCards = [];
-	let cardsInGame = 32;
-	
-	// Checks if two values of Table are equal or not 
-	function cardMatcher() {
-		if (choosenCards.length != 2 ) {
-			let cardID = this.getAttribute('card-id');
-			let inner = this.innerHTML;
-			let innerHtml = this.getAttribute('inner');
-			if (inner != 'True') {
-				choosenCards.push(innerHtml);
-				choosenId.push(cardID);
-				if (choosenCards.length == 2) {
-					setTimeout( function () { 
-						let firstCard = choosenCards[0];
-						let secondCard = choosenCards[1];
-						let id1 = choosenId[0];
-						let id2 = choosenId[1];
-						if (firstCard == secondCard) {
-							trueChoosenCards++;
-							//Changes the value of found numbers to its own value.
-							let divs2 = document.getElementsByTagName('div'), i = divs2.length;
-							while(--i) {
-								if (divs2[i].getAttribute('inner') == choosenCards[0]) {
-									divs2[i].innerHTML = choosenCards[0];
-								}
-							}
-							document.getElementById('Trues').innerHTML = trueChoosenCards;
-							alert('Trueeee :)');
-						} else {
-							alert('Not True :( ')
-						}
-						// make arrays empty for the next step
-						choosenCards = []
-						choosenId = []
-						//wining message 
-						if (cardsInGame == trueChoosenCards) {
-							alert('You Won!');
-						}
-					}, 300);
-				}
-			}
-		}	
-	}
+// Fetch API to get the number of row and column
+async function chartNumberFunc() {
+  const chartNumber = await fetch('http://192.168.43.126:8000/playgame')
+    .then((response) => response.json())
+    .then((data) => data.dimension)
+    .catch((err) => (err));
+  return chartNumber;
 }
 
+// Array of numbers for Game
+const gameValues = [];
+for (let i = 1; i <= 32; i += 1) {
+  gameValues.push(i);
+  gameValues.push(i);
+}
 
+// shuffle Array
+gameValues.sort(() => 0.5 - Math.random());
+const card = document.createElement('div');
+card.setAttribute('id', 'game');
+document.getElementById('box').appendChild(card);
+const newHalfTwo = gameValues.slice();
 
+let choosenCards = [];
+let choosenId = [];
+let trueChoosenCards = [];
+// const cardsInGame = 32;
 
+// Checks if two values of Table are equal or not
+function cardMatcher() {
+  if (choosenCards.length !== 2) {
+    const cardID = this.getAttribute('card-id');
+    const inner = this.innerHTML;
+    const innerHtml = this.getAttribute('inner');
+    if (inner !== 'True') {
+      choosenCards.push(innerHtml);
+      choosenId.push(cardID);
+      if (choosenCards.length === 2) {
+        setTimeout(() => {
+          const firstCard = choosenCards[0];
+          const secondCard = choosenCards[1];
+          if (firstCard === secondCard) {
+            trueChoosenCards += trueChoosenCards;
+            // Changes the value of found numbers to its own value.
+            const divs2 = document.getElementsByTagName('div'); let
+              i = divs2.length;
+            while (i >= 0) {
+              i -= 1;
+              if (divs2[i].getAttribute('inner') === choosenCards[0]) {
+                divs2[i].innerHTML = firstCard;
+              }
+            }
+            document.getElementById('Trues').innerHTML = trueChoosenCards;
+            // alert('Trueeee :)');
+          }
+          // make arrays empty for the next step
+          choosenCards = [];
+          choosenId = [];
+          // wining message
+          // if (cardsInGame == trueChoosenCards) {
+          //  alert('You Won!');
+          // }
+        }, 300);
+      }
+    }
+  }
+}
 
+// Generate Table with 8*8 squares
+async function tableGenerator() {
+  const chartNumber = await chartNumberFunc();
+  for (let i = 1; i <= chartNumber; i += 1) {
+    const cardItem = document.createElement('div');
+    cardItem.setAttribute('class', 'cardColumn');
+    cardItem.setAttribute('card-id', i);
+    card.appendChild(cardItem);
+    for (let j = 0; j < chartNumber; j += 1) {
+      const uniq = `id${(new Date()).getTime()}`;
+      const arrLastElement = newHalfTwo.pop();
+      const cardItemTwo = document.createElement('div');
+      cardItemTwo.setAttribute('class', 'cardRow');
+      cardItemTwo.setAttribute('card-id', uniq);
+      cardItemTwo.setAttribute('id', 'cr');
+      cardItemTwo.setAttribute('inner', arrLastElement);
+      cardItemTwo.addEventListener('click', cardMatcher);
+      cardItemTwo.innerHTML = arrLastElement;
+      cardItem.appendChild(cardItemTwo);
+    }
+  }
+}
 
+tableGenerator();
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// Change inner Value of each square to not be visible
+setTimeout(() => {
+  const divs = document.getElementsByTagName('div'); let
+    i = divs.length;
+  while (i >= 0) {
+    i -= 1;
+    if (divs[i].getAttribute('class') === 'cardRow') {
+      divs[i].innerHTML = '?';
+    }
+  }
+}, 5000);
