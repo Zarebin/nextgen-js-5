263a272d4aa861518507d2c587ef679462787b13
mobina <esmaeilimobina98@gmail.com>
2023-01-14 21:25:03 +0330

    edited webpack and add add eslint2

--- projects/TicToc/webcomponent/webpack.config.js
+++ projects/TicToc/webcomponent/webpack.config.js
@@ -1,6 +1,6 @@
 const path =require('path');
 module.exports={
-	entry:'./src/index.js',
+	entry:'./src/index.ts',
 	devtool: 'inline-source-map',
 	output: {
 		path: path.resolve(__dirname ,'./dist'),
